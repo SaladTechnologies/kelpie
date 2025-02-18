a83986dce8a58c07f4bd51e1ddc1eb4aae79fd98
Shawn <shawn.rushefsky@gmail.com>
2025-02-18 15:43:57 -0500

    0.5.1 - Better handling of transient network failures

--- src/index.ts
+++ src/index.ts
@@ -166,7 +166,10 @@ async function main() {
         /**
          * If there are no uploads in progress, we can reallocate the instance.
          */
-        await reallocateMe(baseLogger);
+        await reallocateMe(
+          `Kelpie: Max idle time exceeded: ${maxTimeWithNoWorkMs}`,
+          baseLogger
+        );
         break;
       }
       baseLogger.info("No work available, sleeping for 10 seconds...");
@@ -183,12 +186,41 @@ async function main() {
 
     const directoryWatchers: DirectoryWatcher[] = [];
 
-    heartbeatManager.startHeartbeat(work.heartbeat_interval, async () => {
+    /**
+     * The heartbeat endpoint may return a status of "canceled" if the job has been cancelled,
+     * in which case we should stop the job and ask for a new one.
+     */
+    async function onJobCancel() {
       await Promise.all(
         directoryWatchers.map((watcher) => watcher.stopWatching())
       );
       commandExecutor.interrupt();
-    });
+    }
+
+    heartbeatManager
+      .startHeartbeat(work.heartbeat_interval, onJobCancel)
+      .catch(async (e: any) => {
+        /**
+         * This occurs if a heartbeat fails config.maxRetries times, meaning the machine
+         * has lost communication with kelpie
+         *  */
+        log.error(`Heartbeat error: ${e.message}`);
+
+        /**
+         * If the heartbeat throws an error, we should restart it.
+         * This is because the error is likely due to a network issue which
+         * may be transient, and the job is still running. This way,
+         * the job can continue to run and the heartbeat will be re-established.
+         *
+         * The alternative is to abort the job or reallocate the instance, but this is
+         * not ideal because the job is still running and may complete successfully.
+         */
+        await heartbeatManager.stopHeartbeat();
+        await heartbeatManager.startHeartbeat(
+          work.heartbeat_interval,
+          onJobCancel
+        );
+      });
 
     /**
      * This block is event-driven, triggered by file changes in configured directories.
